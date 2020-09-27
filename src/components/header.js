9946767b1b880e2760f8c7a89690eb82a9f34442
Michał Marchewczyk <dev@marchewczyk.eu>
2020-09-27 16:35:55 +0200

    Create header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,24 @@
 import {Link} from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import logo from '../images/logo.png';
+
+import headerStyles from './header.module.scss'
 
 
 const Header = ({siteTitle}) => (
-    <header>
-        <div>
-            <h1>
-                <Link>
-                    {siteTitle}
-                </Link>
-            </h1>
-        </div>
+    <header className={headerStyles.header}>
+        <Link to='/'>
+            <div className={headerStyles.logo}>
+                <img src={logo} alt={siteTitle}/>
+            </div>
+        </Link>
+        <nav className={headerStyles.nav}>
+            <Link to='docs/install'>Install</Link>
+            <Link to='docs/quick-start'>Documentation</Link>
+            <Link to='docs/examples'>Examples</Link>
+            <a href='https://github.com/michalmarchewczyk/tsnodes'>GitHub</a>
+        </nav>
     </header>
 )
 
